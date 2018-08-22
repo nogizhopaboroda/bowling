964ed8fc8db8c91e39414672e2ec3e67be00b9d6
Alexander Zhukov <al.zhukoff@gmail.com>
2018-08-22 22:58:38 +0200

    add first turn scenario

--- src/components/game-controller/game-generator.test.js
+++ src/components/game-controller/game-generator.test.js
@@ -1,7 +1,7 @@
 import createGame from './game-generator';
 
 describe('multi player generator', () => {
-  const game = createGame(['John', 'Paul'], 2);
+  const game = createGame(['John', 'Peter'], 2);
   it('generates initial state correctly', () => {
     expect(game.next()).toEqual({
        "done": false,
@@ -20,7 +20,7 @@ describe('multi player generator', () => {
            },
            {
              "id": 1,
-             "name": "Paul",
+             "name": "Peter",
              "score": [
                 [ null, null ],
                 [ null, null, null, ]
@@ -30,5 +30,45 @@ describe('multi player generator', () => {
          "turnTotal": 0,
        },
      });
+  });
+
+  it('1 turn, 1 roll of John', () => {
+    const userScores = 3;
+    const { done, value: { currentPlayer, currentTurn, currentRoll, players } } = game.next(userScores);
+    expect(done).toEqual(false)
+    expect(currentPlayer).toEqual(0);
+    expect(currentTurn).toEqual(0);
+    expect(currentRoll).toEqual(1);
+    expect(players[0].score[0]).toEqual([3, null]);
+  });
+
+  it('1 turn, 2 roll of John', () => {
+    const userScores = 7;
+    const { done, value: { currentPlayer, currentTurn, currentRoll, players } } = game.next(userScores);
+    expect(done).toEqual(false)
+    expect(currentPlayer).toEqual(1);
+    expect(currentTurn).toEqual(0);
+    expect(currentRoll).toEqual(0);
+    expect(players[0].score[0]).toEqual([3, 7]);
+  });
+
+  it('1 turn, 1 roll of Peter', () => {
+    const userScores = 2;
+    const { done, value: { currentPlayer, currentTurn, currentRoll, players } } = game.next(userScores);
+    expect(done).toEqual(false)
+    expect(currentPlayer).toEqual(1);
+    expect(currentTurn).toEqual(0);
+    expect(currentRoll).toEqual(1);
+    expect(players[1].score[0]).toEqual([2, null]);
+  });
+
+  it('1 turn, 2 roll of Peter', () => {
+    const userScores = 8;
+    const { done, value: { currentPlayer, currentTurn, currentRoll, players } } = game.next(userScores);
+    expect(done).toEqual(false)
+    expect(currentPlayer).toEqual(0);
+    expect(currentTurn).toEqual(1);
+    expect(currentRoll).toEqual(0);
+    expect(players[1].score[0]).toEqual([2, 8]);
   });
 })
